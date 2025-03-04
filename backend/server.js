fe82410541d5aafd39c2048ffc6c5e42f5b1ddfb
Debayan-2004 <153715293+Debayan-2004@users.noreply.github.com>
2025-03-04 09:48:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ connectDB();
 // Routes
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.REACT_APP_API_URL || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
